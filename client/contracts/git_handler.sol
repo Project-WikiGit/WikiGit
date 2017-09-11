880498928d29b30e607d109ab1d8b74e55015776
Zefram Lou <zeframlou@gmail.com>
2017-09-11 23:48:17 +0800

    Implemented GitHandler.

--- client/contracts/git_handler.sol
+++ client/contracts/git_handler.sol
@@ -3,8 +3,12 @@ pragma solidity ^0.4.11;
 import './main.sol';
 
 contract GitHandler is Module {
-    function GitHandler(address mainAddr) Module(mainAddr) {
+    event ExecuteConsoleCommand(string command);
 
+    function GitHandler(address mainAddr) Module(mainAddr) {}
+
+    function executeConsoleCommand(string command) onlyMod('DAO') {
+        ExecuteConsoleCommand(command);
     }
 
     function() {
